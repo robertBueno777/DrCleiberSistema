e17c0595f2f9c1d279c6b87aacd0501849900be7
robertBueno777 <robert.buenelson@gmail.com>
2026-06-27 01:45:20 -0400

    Configurando o dbcontext e onde faremos a conexão com o banco

--- Data/DrCleiberData.cs
+++ Data/DrCleiberData.cs
@@ -5,5 +5,10 @@ namespace DrCleiberSistema.Data
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<PacienteModel>  Pacientes {get; set;}
+
     }
 }
